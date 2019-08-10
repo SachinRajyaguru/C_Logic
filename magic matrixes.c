6614be6d7ca6eb6001ce38cca15e59d16e7ecbe1
SachinRajyaguru <41114520+SachinRajyaguru@users.noreply.github.com>
2019-08-10 17:32:09 +0530

    Update magic matrixes.c

--- magic matrixes.c
+++ magic matrixes.c
@@ -5,8 +5,6 @@ void main()
     printf("input n for the matix : ");
     scanf("%d",&n);
 
-
-
     /*create matrix by n number*/
     int mat[n][n];
 
@@ -21,13 +19,11 @@ void main()
 
         int row = 0;
         int col = n/2;
-        mat[0][n/2] = 1;
+        mat[row][col] = 1;
 
         for ( int i = 2 ; i <= n*n;  i++)
         {
-            //printf("%d %d",row,col);
-            row--;
-            col++;
+            row--;col++;
 
             if(row < 0 && col >= n)
             {
@@ -37,25 +33,13 @@ void main()
                 continue;
             }
 
-            if(row < 0)
-            {
-                row = n-1;
-                if(mat[row][col] == 0)
-                {
-                    mat[row][col] = i;
-                    continue;
-                }
+            row = row < 0 ? n-1 : row;
+            col = col  > n-1 ? 0 : col;
 
-            }
-            if(col  > n-1)
+            if(mat[row][col] == 0)
             {
-                col = 0;
-
-                if(mat[row][col] == 0)
-                {
-                    mat[row][col] = i;
-                    continue;
-                }
+                mat[row][col] = i;
+                continue;
             }
 
             if(mat[row][col] != 0 )
@@ -68,22 +52,16 @@ void main()
 
             mat[row][col] = i;
         }
-
-        printf("\n\n");
-        /*print the matrix*/
+        int sum ;
         for ( int i = 0 ; i < n ; i++)
         {
+            sum = 0;
             for ( int j = 0 ; j < n ; j++)
             {
-                printf("%3d | ",mat[i][j]);
+                sum+= mat[0][j];
+                printf(" |%2d| ",mat[i][j]);
             }
-            printf("\n\n");
-        }
-        printf("\n\n");
-        int sum = 0;
-        for ( int i = 0 ; i < n ; i++)
-        {
-            sum+= mat[0][i];
+            printf("\n");
         }
         printf("total = %3d",sum);
     }
